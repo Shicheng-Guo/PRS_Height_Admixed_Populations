04cddcb321d04b9401770f1f75e73806a5374386
bbitarello <barbara.bitarello@pennmedicine.upenn.edu>
2019-05-17 18:30:16 -0400

    local_anc_gwas

--- gwas/ukb_afr/scripts/run_AS_GWAS.R
+++ gwas/ukb_afr/scripts/run_AS_GWAS.R
@@ -9,6 +9,7 @@ library("optparse")
 ## Rough outline of ancestry-specific GWAS analysis.
 library(readr)
 library(data.table)
+library(dplyr)
 #args<-22
 what <- paste0("~/height_prediction/gwas/ukb_afr/input/UKB_kgCY_chr", args[1])
 
@@ -27,8 +28,7 @@ snp<-fread(paste0(what, ".phsnp"))
 #samples.to.include <- ind[,3]=="Case"
 exclude.samples<-grep("\\bNA",ind$V1)
 ind <- ind[-exclude.samples,]
-setDT(geno)
-geno[,colnames(geno)[exclude.samples]:=NULL]
+geno<-geno[,-exclude.samples]
 #setDT(ancestry)
 #ancestry[, paste0("X", exclude.samples):=NULL]
 
@@ -36,8 +36,9 @@ if(!all(dim(geno)==dim(ancestry))){
     stop("Genotype and ancestry matrices different sizes")
 }
 
+
 ## Load and merge phenotypes
-pheno <- read.table("UKB_AFR_pheno.txt", as.is=TRUE, header=TRUE)
+pheno<-read.table("~/height_prediction/runSmartpCA-master/UKB_AFR/UKB_AFR_pheno.txt", as.is=TRUE, header=TRUE)
 rownames(pheno) <- pheno$ID
 ind <- ind[,c(1,2)]
 colnames(ind) <- c("HapID", "Sex")
@@ -46,6 +47,37 @@ ind$SUBJID<-gsub("_[AB]", "",gsub("[0-9]+:", "", ind[,1]))
 ind$HEIGHTX <- pheno[as.character(ind$SUBJID),"Height"]
 ind$AGE <- pheno[as.character(ind$SUBJID),"Age"]
 ind$AGE2 <- ind$AGE^2
+setDT(ind)
+
+
+#read in PCs for ukb_afr
+pcs_A<-fread('~/height_prediction/runSmartpCA-master/UKB_AFR/tmp')
+pcs_A<-pcs_A[,.(SUBJID, PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10)]
+pcs_A<-arrange(pcs_A, SUBJID) %>% as.data.table
+
+pcs_A[,SUBJID:=gsub("[0-9]+_", "", pcs_A$SUBJID)]
+pcs_A<-pcs_A[, HapID:=paste0(SUBJID, ":", SUBJID, "_A")]
+
+pcs_B<-fread('~/height_prediction/runSmartpCA-master/UKB_AFR/tmp')
+pcs_B<-pcs_B[,.(SUBJID, PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10)]
+pcs_B<-arrange(pcs_B, SUBJID) %>% as.data.table
+
+
+pcs_B[,SUBJID:=gsub("[0-9]+_", "", pcs_B$SUBJID)]
+pcs_B<-pcs_B[, HapID:=paste0(SUBJID, ":", SUBJID, "_B")]
+
+
+pcs<-setDT(arrange(rbind(pcs_A,pcs_B), SUBJID))
+setkey(pcs, HapID, SUBJID)
+
+pcs<-pcs[which(pcs$SUBJID %in% ind$SUBJID),]# remove 4 liens that don't match
+setkey(ind, HapID, SUBJID)
+setkey(pcs, HapID, SUBJID)
+ind<-ind[pcs]
+
+geno[,-which(is.na(ind$HEIGHTX))]-> geno #tremove people without height info
+ancestry[,-which(is.na(ind$HEIGHTX))]-> ancestry
+ind[-which(is.na(ind$HEIGHTX)),]-> ind
 
 N.snps <- dim(geno)[1]
 N.haps <- dim(geno)[2]
@@ -53,36 +85,34 @@ N.haps <- dim(geno)[2]
 #Results will be the regression betas 
 betas <- data.frame(ALL=rep(0, N.snps), POP1=rep(0,N.snps), POP2=rep(0, N.snps), SNP_ID=snp$V1, POS=snp$V4, CHR=snp$V2) #I think POP1 is AFR
 
-#read in PCs for ukb_afr
-
-#
-#
-#
-#
-#
-#
-#
-#
-#
-
-#Now iterate over SNPS
 for(i in 1:N.snps){
     cat(paste0("\r", i))
     
     #ALL GWAS
     gt <- unlist(geno[i,])
-    model.all <- lm(ind$HEIGHTX~gt+ind$Sex+ind$AGE+ind$AGE2)
-    betas[i,1] <- summary(model.all)$coefficients[12]
+    if(sum(gt)==0 ||sum(gt)==length(gt)){ #if all genotypes are the same, the mdoel won't produce a beta for gt
+	betas[i,1]<-NA
+	} else{	 
+    		model.all <- lm(ind$HEIGHTX~gt+ind$Sex+ind$AGE+ind$AGE2+ind$PC1+ind$PC2+ind$PC3+ind$PC4+ind$PC5+ind$PC6+ind$PC6+ind$PC7+ind$PC8+ind$PC9+ind$PC10)
+    		betas[i,1] <- summary(model.all)$coefficients['gt','Estimate']
+    		#Ancestry specific GWAS
+	}
 
-    #Ancestry specific GWAS
-
-    include.1 <- unlist(ancestry[i,])==1
-    include.2 <- unlist(ancestry[i,])==2
-    model.1 <- lm(ind$HEIGHTX~gt+ind$Sex+ind$AGE+ind$AGE2, subset=include.1)
-    model.2 <- lm(ind$HEIGHTX~gt+ind$Sex+ind$AGE+ind$AGE2, subset=include.2)
-    betas[i,2] <- summary(model.1)$coefficients[12]
-    betas[i,3] <- summary(model.2)$coefficients[12]
+	include.1 <- unlist(ancestry[i,])==1
+    	include.2 <- unlist(ancestry[i,])==2
+	if(sum(gt[include.1])==0 || sum(gt[include.1])==length(gt[include.1])){
+	       betas[i,2]<-NA
+	} else {
+		model.1 <- lm(ind$HEIGHTX~gt+ind$Sex+ind$AGE+ind$AGE2+ind$PC1+ind$PC2+ind$PC3+ind$PC4+ind$PC5+ind$PC6+ind$PC6+ind$PC7+ind$PC8+ind$PC9+ind$PC10, subset=include.1)
+		betas[i,2] <- summary(model.1)$coefficients['gt','Estimate']
+	}
+	if(sum(gt[include.2])==0 || sum(gt[include.2])==length(gt[include.2])){
+        	betas[i,3]<-NA
+	} else {
+    		model.2 <- lm(ind$HEIGHTX~gt+ind$Sex+ind$AGE+ind$AGE2+ind$PC1+ind$PC2+ind$PC3+ind$PC4+ind$PC5+ind$PC6+ind$PC6+ind$PC7+ind$PC8+ind$PC9+ind$PC10, subset=include.2)
+    		betas[i,3] <- summary(model.2)$coefficients['gt','Estimate']
+	}
 }
+write.table(betas, paste0("~/height_prediction/gwas/ukb_afr/output/AS_Beta_chr",  args[1],"example.txt"), row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
 
-write.table(betas, paste0("/project/mathilab/bbita/gwas_admix/new_height/ukb_afr/AS_Beta_chr",  args[1],"example.txt"), row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
 
